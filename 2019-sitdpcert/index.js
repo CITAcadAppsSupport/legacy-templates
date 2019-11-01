c6e3c73dbc7f54786a6cb7d2c0a18936738b8264
SIT Apps Team <50308837+CITAcadAppsSupport@users.noreply.github.com>
2019-11-01 15:26:09 +0800

    Add files via upload
    new ou template

---
+++ 2019-sitdpcert/index.js
@@ -0,0 +1,17 @@
+import MultiCertificateRenderer from "template-utils/MultiCertificateRenderer";
+import Certificate from "./certificate";
+import  SITAddress from "../common/address";
+
+const templates = [
+  {
+    id: "certificate",
+    label: "Certificate",
+    template: Certificate
+  }
+]; 
+
+const SITCert = () => (
+  <MultiCertificateRenderer templates={templates} whitelist={SITAddress} />
+);
+
+export default SITCert;
